1d987c83c393b166d36a65d758638f4d15562aa6
Kanunnikov Sergey <SergeyKanunnikov@MacBook-Pro-Kanunnikov.local>
2017-08-24 22:33:55 +0300

    Class update, objects

--- python_homework_3_6.py
+++ python_homework_3_6.py
@@ -1,35 +1,47 @@
-class Animal():
-    noise = None
-    bio_class = "Mammal" or "Bird"
-    name = None
+class Animal:
+    def __init__(self, bio_class, name, sounds):
+        self.bio_class = bio_class
+        self.name = name
+        self.sounds = sounds
 
     def check_bio_class(self):
         print(self.bio_class)
 
     def make_noise(self):
-        print(self.noise)
+        print("{} says {}".format(self.name, self.sounds))
 
-    def set_animal_name(self):
-        self.name = input("Введите имя:\n")
-        return Animal.name
+    def check_animal_name(self):
+        print(self.name)
 
 
 class Cow(Animal):
     bio_class = "Mammal"
-    name = "Mumu"
-    noise = "Mooo"
+    sounds = "Mooo"
 
 
 class Goose(Animal):
-    bio_class = "123"
-    noise = "Quack"
-    name = Animal.set_animal_name()
+    bio_class = "Bird"
+    sounds = "Quack"
 
 
-cow_1 = Cow()
-goose_1 = Goose()
-
-# goose_1.set_animal_name()
-
+class Pig(Animal):
+    bio_class = "Mammal"
+    sounds = "Oink"
 
 
+class Chicken(Animal):
+    bio_class = "Bird"
+    sounds = "Cluck"
+
+cow_1 = Cow(Cow.bio_class, "Buryonka", Cow.sounds)
+
+cow_1.check_bio_class()
+cow_1.check_animal_name()
+cow_1.make_noise()
+
+
+goose_1 = Cow(Goose.bio_class, "Gaga", Goose.sounds)
+
+goose_1.check_bio_class()
+goose_1.check_animal_name()
+goose_1.make_noise()
